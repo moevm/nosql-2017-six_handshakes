47f6cfd4103bd2bfaff19485418a4b28f269d4be
Sergey <serezha.karpunin@gmail.com>
2017-09-14 04:57:44 +0400

    Added annotated fields to Person class

--- src/main/java/com/eltech/sh/model/Person.java
+++ src/main/java/com/eltech/sh/model/Person.java
@@ -1,6 +1,78 @@
 package com.eltech.sh.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.neo4j.ogm.annotation.GraphId;
+import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Relationship;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@NodeEntity
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Person {
+
+    @GraphId
+    @JsonIgnore
+    private Long id;
+
+    @JsonProperty("first_name")
+    private String firstName;
+
+    @JsonProperty("last_name")
+    private String lastName;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * Neo4j doesn't REALLY have bi-directional relationships. It just means when querying
+     * to ignore the direction of the relationship.
+     * https://dzone.com/articles/modelling-data-neo4j
+     */
+    @Relationship(type = "FRIEND", direction = Relationship.UNDIRECTED)
+    @JsonIgnore
+    public Set<Person> friends;
+
+
+
+    public void friendOf(Person person) {
+        if (friends == null) {
+            friends = new HashSet<>();
+        }
+        friends.add(person);
+    }
+
+//    public String toString() {
+//
+//        return this.name + "'s teammates => "
+//                + Optional.ofNullable(this.teammates).orElse(
+//                Collections.emptySet()).stream().map(
+//                person -> person.getName()).collect(Collectors.toList());
+//    }
 
 }
