22b849758a50efb2d6dee936d6ee75391d5bbee3
Sergey <serezha.karpunin@gmail.com>
2017-09-14 15:36:11 +0400

    Replaced string.replace() methods by objectMapper calls

--- src/main/java/com/eltech/sh/service/VKService.java
+++ src/main/java/com/eltech/sh/service/VKService.java
@@ -2,7 +2,9 @@ package com.eltech.sh.service;
 
 import com.eltech.sh.configuration.VkCredentialsConfiguration;
 import com.eltech.sh.model.Person;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vk.api.sdk.actions.Friends;
 import com.vk.api.sdk.client.VkApiClient;
@@ -14,6 +16,7 @@ import com.vk.api.sdk.objects.users.UserXtrCounters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
 
@@ -21,24 +24,30 @@ import java.util.List;
 @Service
 public class VKService {
 
+    private final HttpSession session;
     private final VkCredentialsConfiguration configuration;
     private final VkApiClient vkApiClient;
     private final Friends friends;
+    private final ObjectMapper objectMapper;
 
     @Autowired
-    public VKService(VkCredentialsConfiguration configuration, VkApiClient vkApiClient, Friends friends) {
+    public VKService(VkCredentialsConfiguration configuration, VkApiClient vkApiClient, Friends friends, ObjectMapper objectMapper, HttpSession session) {
         this.configuration = configuration;
         this.vkApiClient = vkApiClient;
         this.friends = friends;
+        this.objectMapper = objectMapper;
+        this.session = session;
     }
 
-    private UserActor getUserActor(String token){
-       return new UserActor(configuration.getAppId(), token);
+    private UserActor getUserActor() {
+        return new UserActor(configuration.getAppId(), (String) session.getAttribute("access_token"));
     }
 
-    private UserXtrCounters getUserById(String userId, String token) {
+
+
+    public UserXtrCounters getUserById(String userId) {
         try {
-            List<UserXtrCounters> list = vkApiClient.users().get(getUserActor(token)).userIds(userId).execute();
+            List<UserXtrCounters> list = vkApiClient.users().get(getUserActor()).userIds(userId).execute();
             return list.get(0);
         } catch (ApiException | ClientException e) {
             e.printStackTrace();
@@ -46,28 +55,27 @@ public class VKService {
         return null;
     }
 
-    public List<Person> findFriend(String userStringId, String token) {
-        Integer userId = getUserById(userStringId, token).getId();
-        String resp = null;
+
+    public List<Person> findPersonFriends(String userStringId) {
+        Integer userId = getUserById(userStringId).getId();
+        String response = null;
         try {
-            resp = friends.get(getUserActor(token)).listId(userId).unsafeParam("fields", "city,domain").unsafeParam("user_id", userId).executeAsRaw().getContent();
+            response = friends.get(getUserActor()).listId(userId).unsafeParam("fields", "city,domain").unsafeParam("user_id", userId).executeAsRaw().getContent();
         } catch (ClientException e) {
             e.printStackTrace();
         }
 
-        //FIXME fix this hardcode
-        resp = resp.replace("{\"response\":{\"count\":60,\"items\":", "");
-        resp = resp.replace("]}}", "]");
+        List<Person> convertedFriends = null;
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        List<Person> parsedFriends = null;
         try {
-            parsedFriends = objectMapper.readValue(resp, new TypeReference<List<Person>>(){});
+            JsonNode jsonNode = objectMapper.readTree(response).path("response").path("items");
+            convertedFriends = objectMapper.convertValue(jsonNode, new TypeReference<List<Person>>() {
+            });
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return parsedFriends;
+        return convertedFriends;
     }
 
 
