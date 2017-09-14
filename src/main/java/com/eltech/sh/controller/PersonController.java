0fdbbd61d92072ca5f2c19681764902f5239ca75
Sergey <serezha.karpunin@gmail.com>
2017-09-14 04:58:33 +0400

    Added endpoint to Person controller

--- src/main/java/com/eltech/sh/controller/PersonController.java
+++ src/main/java/com/eltech/sh/controller/PersonController.java
@@ -1,14 +1,39 @@
 package com.eltech.sh.controller;
 
+import com.eltech.sh.model.Person;
+import com.eltech.sh.repository.PersonRepository;
+import com.eltech.sh.service.VKService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
 public class PersonController {
 
-    @GetMapping("/test")
-    String test(){
-        return "test";
+    private final VKService vkService;
+    private final PersonRepository personRepository;
+
+    @Autowired
+    public PersonController(VKService vkService, PersonRepository personRepository) {
+        this.vkService = vkService;
+        this.personRepository = personRepository;
     }
+
+    //FIXME fix hardcode
+    @GetMapping("/persons/{personId}/friends")
+    List<Person> getPersonFriends(@PathVariable("personId") String id, HttpServletRequest request) {
+        String token = (String) request.getSession().getAttribute("access_token");
+        List<Person> friends = vkService.findFriend(id, token);
+
+        Person user = new Person("Serezha", "Karpunin");
+        friends.forEach(user::friendOf);
+        personRepository.save(user);
+        return friends;
+    }
+
 
 }
