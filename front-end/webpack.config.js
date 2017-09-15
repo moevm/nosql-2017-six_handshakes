5c9d64fa0b6d370f9de5fc26726fc3993a6dd8d1
Sergei_Karpunin <Sergei_Karpunin@epam.com>
2017-09-15 18:27:16 +0300

    Added jsx support

--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -6,7 +6,15 @@ module.exports = {
     },
     module: {
         loaders: [
-            {test: /\.css$/, loader: "style-loader!css-loader"}
+            {test: /\.css$/, loader: "style-loader!css-loader"},
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
         ]
     }
 };
