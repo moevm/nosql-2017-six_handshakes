d5f7acb67d019fa749421172190d08149d054fe8
Sergei_Karpunin <Sergei_Karpunin@epam.com>
2017-09-15 13:09:47 +0300

    Added html form and controller for supporting it

--- src/main/java/com/eltech/sh/Application.java
+++ src/main/java/com/eltech/sh/Application.java
@@ -1,7 +1,5 @@
 package com.eltech.sh;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
@@ -9,8 +7,6 @@ import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 @SpringBootApplication
 @EnableNeo4jRepositories
 public class Application {
-    private final static Logger log = LoggerFactory.getLogger(Application.class);
-
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
