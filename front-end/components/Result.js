a427cb096d4fd9e5910217cb97defd9c024b9802
Sergey <serezha.karpunin@gmail.com>
2017-09-22 15:54:32 +0400

    Minor fixes in js/css files

--- front-end/components/Result.js
+++ front-end/components/Result.js
@@ -2,7 +2,10 @@ import React from "react";
 export const Result = ({result}) => {
 
     const list = result.map(elem =>
-        <p>{elem.firstName} {elem.lastName} -> </p>
+        <div>
+            <div className='image-round' style={{backgroundImage: `url(${elem.photoUrl})`}}/>
+            <p>{elem.firstName} {elem.lastName} -> </p>
+        </div>
     );
 
     return (
--- front-end/components/StatusBar.js
+++ front-end/components/StatusBar.js
@@ -2,7 +2,6 @@ import React from "react";
 export class StatusBar extends React.Component {
     render() {
         const {socketState, searchState} = this.props.socket;
-        console.log(searchState);
         const list = searchState.map((item, index) => <li key={index}>{item}</li>);
         return (
             <div>
