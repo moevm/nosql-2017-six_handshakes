23fe6c151d5ef2ad8fe7eeedbc0a65b1b4a4eb19
Sergey <serezha.karpunin@gmail.com>
2017-10-04 21:33:11 +0400

    Added switching between different datasources

--- front-end/components/Form.js
+++ front-end/components/Form.js
@@ -1,19 +1,22 @@
 import React from "react";
-import {reduxForm, Field} from 'redux-form';
+import {reduxForm, Field, change} from 'redux-form';
 import {handleFormSubmit} from "../actions/GraphActions";
-
+import "./Form.css"
+import {connect} from "react-redux";
 let Form = props => {
-    const {handleSubmit, socket: {socketState, searchState}} = props;
+    const {handleSubmit, socket: {socketState, searchState}, setDataSource, formValues: {dataSource}} = props;
     const disabled = socketState !== 'CONNECTED' || searchState.length !== 0;
     return (
         <form className="main-form" onSubmit={handleSubmit}>
             <div>
                 <h3>Choose data source</h3>
-                <div className="icon-button">
+                <div className={`icon-button vk ${dataSource === 'VK' ? 'active' : ''}`}
+                     onClick={() => setDataSource('VK')}>
                     <i className="fa fa-vk "/>
                     vkontakte
                 </div>
-                <div className="icon-button">
+                <div className={`icon-button ${dataSource === 'FILE' ? 'active' : ''}`}
+                     onClick={() => setDataSource('FILE')}>
                     <i className="fa fa-upload"/>
                     your data
                 </div>
@@ -40,9 +43,24 @@ let Form = props => {
     )
 };
 
+
+Form = connect(
+    state => ({
+        formValues: state.form.mainForm.values
+    }),
+    dispatch => ({
+        setDataSource: (value) => dispatch(change('mainForm', 'dataSource', value))
+    })
+)(Form);
+
 Form = reduxForm({
     form: 'mainForm',
-    onSubmit: handleFormSubmit
+    onSubmit: handleFormSubmit,
+    initialValues: {
+        from: '',
+        to: '',
+        dataSource: 'VK'
+    }
 })(Form);
 
 export default Form;
