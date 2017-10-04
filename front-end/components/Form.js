fbd9a966b7ba99e5c4749e921bcaa6516f772320
Sergey <serezha.karpunin@gmail.com>
2017-10-05 02:46:03 +0400

    Added error label to form

--- front-end/components/Form.js
+++ front-end/components/Form.js
@@ -5,31 +5,40 @@ import "./Form.css"
 import {connect} from "react-redux";
 let Form = props => {
     const {handleSubmit, socket: {socketState, searchState}, setDataSource, formValues: {dataSource, from, to}} = props;
-    const disabled = !from || !to || socketState !== 'CONNECTED' || searchState.length !== 0;
+
+    let errorMessage = '';
+    if (!(from && to)) errorMessage = 'IDs shouldn\'t be empty';
+    if (socketState !== 'CONNECTED') errorMessage = 'Connecting to server...';
+
     return (
-        <form className="main-form" onSubmit={handleSubmit}>
-            <h3>Choose data source</h3>
-            <div className="row">
-                <div className={`icon-button vk ${dataSource === 'VK' ? 'active' : ''}`}
-                     onClick={() => setDataSource('VK')}>
-                    <i className="fa fa-vk "/>
-                    vkontakte
+        <div>
+            <form className="main-form content-wrapper" onSubmit={handleSubmit}>
+                <h3>Choose data source</h3>
+                <div className="row">
+                    <div className={`icon-button vk ${dataSource === 'VK' ? 'active' : ''}`}
+                         onClick={() => setDataSource('VK')}>
+                        <i className="fa fa-vk "/>
+                        vkontakte
+                    </div>
+                    <div className={`icon-button ${dataSource === 'FILE' ? 'active' : ''}`}
+                         onClick={() => setDataSource('FILE')}>
+                        <i className="fa fa-upload"/>
+                        your data
+                    </div>
                 </div>
-                <div className={`icon-button ${dataSource === 'FILE' ? 'active' : ''}`}
-                     onClick={() => setDataSource('FILE')}>
-                    <i className="fa fa-upload"/>
-                    your data
+
+                <h3>Enter IDs</h3>
+                <div className="row">
+                    <Field name="from" component="input" type="text" placeholder="from"/>
+                    <Field name="to" component="input" type="text" placeholder="to"/>
                 </div>
-            </div>
 
-            <h3>Enter IDs</h3>
-            <div className="row">
-                <Field name="from" component="input" type="text" placeholder="from"/>
-                <Field name="to" component="input" type="text" placeholder="to"/>
-            </div>
-
-            <button className={`submit-button ${(disabled) ? 'disabled' : ''}`} type="submit" disabled={disabled}>Check</button>
-        </form>
+                <button className={`submit-button ${(errorMessage) ? 'disabled' : ''}`} type="submit"
+                        disabled={errorMessage}>Check
+                </button>
+            </form>
+            {errorMessage && <div className="error-label"><i className="fa fa-exclamation-triangle"/>{errorMessage}</div>}
+        </div>
     )
 };
 
