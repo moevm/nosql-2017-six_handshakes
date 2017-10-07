803f9487faab4ccadc4d305988e03ec885f282ac
Sergey <serezha.karpunin@gmail.com>
2017-10-07 19:12:29 +0400

    Added details panel

---
+++ front-end/components/DetailsPanel.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {Legend, Pie, PieChart} from "recharts";
+import {formatTime} from "../utils/mathUtils";
+export class DetailsPanel extends React.Component {
+    render() {
+        if (this.props.data) {
+            const {peopleChecked, currentWeb} = this.props.data;
+            return (
+                <div className="details-panel">
+                    <h1>Details</h1>
+                    <div className="numbers">
+                        <div><i className="fa fa-2x fa-male icon"/> Checked persons: {peopleChecked}</div>
+                        <div><i className="fa fa-2x fa-male icon"/> Total in database: {currentWeb}</div>
+                    </div>
+                </div>
+            )
+        } else {
+            return <div/>;
+        }
+
+    }
+}
+
