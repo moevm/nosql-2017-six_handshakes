6d013b66f27984a7092f3024e730c16f58b07ba7
Ekaterina Nikitina <katyanic@mail.ru>
2017-10-21 19:42:11 +0300

    50 Added exception handler

--- back-end/src/main/java/com/eltech/sh/controller/PersonController.java
+++ back-end/src/main/java/com/eltech/sh/controller/PersonController.java
@@ -4,11 +4,16 @@ import com.eltech.sh.beans.ResponseBean;
 import com.eltech.sh.model.Person;
 import com.eltech.sh.service.CoreService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.constraints.Null;
+import javax.xml.bind.ValidationException;
 
 
 @RestController
@@ -30,6 +35,11 @@ public class PersonController {
     @GetMapping("/find")
     public ResponseBean checkSixHandshakes(@RequestParam("from") String fromId, @RequestParam("to") String toId, HttpServletRequest request) {
         String currentUserId = (String) request.getSession().getAttribute("current_user_id");
-        return coreService.run(fromId, toId,currentUserId);
+        return coreService.run(fromId, toId, currentUserId);
+    }
+
+    @ExceptionHandler({ValidationException.class, NullPointerException.class})
+    public ResponseEntity<String> handleValidationException(Exception e) {
+        return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
